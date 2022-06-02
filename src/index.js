c905c435b31686a7ad1287a9e17234d6e1b34704
skillazz <skillaz92@gmail.com>
2022-06-02 03:25:35 +0200

    feat: added basic form, prepared layout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
+import {CssBaseline} from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <CssBaseline/>
     <App />
   </React.StrictMode>
 );
